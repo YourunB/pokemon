9eeb63841667385e685527b17e13d8e5312c4195
yourunb <bxyura@gmail.com>
2025-01-10 16:58:59 +0300

    feat: add page not found

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,17 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PageStart from './src/pages/pageStart';
 import PageAbout from './src/pages/PageAbout';
 import PageHome from './src/pages/PageHome';
+import PageNotFound from './src/pages/PageNotFound';
 
 function App() {
   return (
     <>
-
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<PageStart />} />
           <Route path="/home" element={<PageHome />} />
           <Route path="/about" element={<PageAbout />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/src/pages/PageNotFound.tsx
@@ -0,0 +1,9 @@
+const PageNotFound = () => {
+  return (
+    <>
+      Sorry... This page is not found... 404
+    </>
+  )
+}
+
+export default PageNotFound
