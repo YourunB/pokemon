49232cae336a70bc1bf4267b52e66df072cc78ac
yourunb <bxyura@gmail.com>
2025-01-10 16:56:48 +0300

    feat: add links to pages

--- src/src/pages/PageStart.tsx
+++ src/src/pages/PageStart.tsx
@@ -5,6 +5,10 @@ const PageStart = () => {
     <>
       Page start...
       <img src={pokemonLogo} className="logo react" alt="React logo" />
+      <ul>
+        <li><a href='/home'>Home page</a></li>
+        <li><a href='/about'>About page</a></li>
+      </ul>
     </>
   )
 }
