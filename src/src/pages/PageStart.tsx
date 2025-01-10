07117923d20574206400a65c9859d04ae9e02bca
yourunb <bxyura@gmail.com>
2025-01-10 17:07:52 +0300

    feat: add link to typescript

--- src/src/pages/PageStart.tsx
+++ src/src/pages/PageStart.tsx
@@ -10,7 +10,7 @@ const PageStart = () => {
         <li><a href='/about'>About page</a></li>
       </ul>
       <div>
-        <p>This application is developed for educational purposes using technologies such as <a href='https://react.dev/' target='_blank'>React</a>, <a href='https://redux.js.org/' target='_blank'>Redux</a>, <a href='https://redux-toolkit.js.org/rtk-query/overview' target='_blank'>RTK Query</a>, <a href='https://vite.dev/guide/'>Vite</a> and <a href='https://pokeapi.co/' target='_blank'>PokeAPI</a>. The application is written in TypeScript.</p>
+        <p>This application is developed for educational purposes using technologies such as <a href='https://react.dev/' target='_blank'>React</a>, <a href='https://redux.js.org/' target='_blank'>Redux</a>, <a href='https://redux-toolkit.js.org/rtk-query/overview' target='_blank'>RTK Query</a>, <a href='https://vite.dev/guide/'>Vite</a> and <a href='https://pokeapi.co/' target='_blank'>PokeAPI</a>. The application is written in <a href='https://www.typescriptlang.org/' target='_blank'>TypeScript</a>.</p>
       </div>
     </>
   )
