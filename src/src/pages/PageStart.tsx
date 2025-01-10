58de453fede60710469ca872c1546cd060a1c022
yourunb <bxyura@gmail.com>
2025-01-10 17:06:23 +0300

    feat: add description for start page and links to sources

--- src/src/pages/PageStart.tsx
+++ src/src/pages/PageStart.tsx
@@ -9,6 +9,9 @@ const PageStart = () => {
         <li><a href='/home'>Home page</a></li>
         <li><a href='/about'>About page</a></li>
       </ul>
+      <div>
+        <p>This application is developed for educational purposes using technologies such as <a href='https://react.dev/' target='_blank'>React</a>, <a href='https://redux.js.org/' target='_blank'>Redux</a>, <a href='https://redux-toolkit.js.org/rtk-query/overview' target='_blank'>RTK Query</a>, <a href='https://vite.dev/guide/'>Vite</a> and <a href='https://pokeapi.co/' target='_blank'>PokeAPI</a>. The application is written in TypeScript.</p>
+      </div>
     </>
   )
 }
