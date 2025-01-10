3729c73647aae64f52ebb7df2bd543d07b214d74
Yury Butskevich <124175026+YourunB@users.noreply.github.com>
2025-01-10 16:07:35 +0300

    feat: create app with vite (#1)
    * feat: create vite react app
    
    * feat: add logo ball for pokemon
    
    * feat: add image logo pokemon
    
    * feat: add sass by css
    
    * refactor: change app style

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import pokemonLogo from './assets/images/logo-name.png'
+import './App.sass'
+
+function App() {
+  return (
+    <>
+      <img src={pokemonLogo} className="logo react" alt="React logo" />
+    </>
+  )
+}
+
+export default App
