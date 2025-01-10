dc5624762491dcb3da3e90e73fcc9d2c90a04091
yourunb <bxyura@gmail.com>
2025-01-10 17:13:50 +0300

    refactor: replace a href by Link

--- src/src/pages/PageStart.tsx
+++ src/src/pages/PageStart.tsx
@@ -1,4 +1,5 @@
 import pokemonLogo from '../assets/images/logo-name.png'
+import { Link } from 'react-router-dom'
 
 const PageStart = () => {
   return (
@@ -6,8 +7,8 @@ const PageStart = () => {
       Page start...
       <img src={pokemonLogo} className="logo react" alt="React logo" />
       <ul>
-        <li><a href='/home'>Home page</a></li>
-        <li><a href='/about'>About page</a></li>
+        <li><Link to={'/home'}>Home page</Link></li>
+        <li><Link to={'/about'}>About page</Link></li>
       </ul>
       <div>
         <p>This application is developed for educational purposes using technologies such as <a href='https://react.dev/' target='_blank'>React</a>, <a href='https://redux.js.org/' target='_blank'>Redux</a>, <a href='https://redux-toolkit.js.org/rtk-query/overview' target='_blank'>RTK Query</a>, <a href='https://vite.dev/guide/'>Vite</a> and <a href='https://pokeapi.co/' target='_blank'>PokeAPI</a>. The application is written in <a href='https://www.typescriptlang.org/' target='_blank'>TypeScript</a>.</p>
