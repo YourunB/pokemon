564193e7a0b7f88996a23bda0ea4efe0c5ae9624
yourunb <bxyura@gmail.com>
2025-01-11 14:07:14 +0300

    feat: add type for pokemon data

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,20 @@
 import React, { useState } from 'react';
 import { useFetchPokemonQuery, useFetchPokemonListQuery } from '../../store/apiSlice';
 
+type TPokemonData = {
+  name: string;
+  height: number;
+  weight: number;
+  sprites: {
+    front_default: string;
+  };
+}
+
+
 const PageHome: React.FC = () => {
   const [pokemonName, setPokemonName] = useState<string>('pikachu');
-  const [page, setPage] = useState<number>(0)
-  
+  const [page, setPage] = useState<number>(0);
+
   const { data: pokemonList, error: listError, isLoading: isLoadingList } = useFetchPokemonListQuery(page * 20);
   const { data: pokemonData, error: pokemonError, isLoading: isLoadingPokemon } = useFetchPokemonQuery(pokemonName);
 
@@ -14,20 +24,26 @@ const PageHome: React.FC = () => {
 
   const handlePageChange = (n: number) => {
     const resultPage = page + n;
-    if (resultPage >= 0) setPage(resultPage)
-  }
+    if (resultPage >= 0) setPage(resultPage);
+  };
 
   if (isLoadingList) return <div>Loading Pokemon List...</div>;
   if (listError) return <div>Error loading list: {listError.message}</div>;
 
-  if (isLoadingPokemon) return <div>Loading {pokemonName}...</div>;
-  if (pokemonError) return <div>Error loading {pokemonName}: {pokemonError.message}</div>;
-console.log('render', pokemonList)
+  console.log('render', pokemonList);
+  console.log('search', pokemonData);
+
   return (
     <div>
+      <div>
+        <h3>Pokemon:</h3>
+        <input value={pokemonName} onChange={(e) => setPokemonName(e.target.value)} />
+        {pokemonError && <div>not found pokemon...</div>}
+      </div>
+
       <h2>Pokemon List:</h2>
       <ul>
-        {pokemonList?.results?.map((pokemon: any) => (
+        {pokemonList?.results?.map((pokemon: TPokemonData) => (
           <li key={pokemon.name}>
             <button onClick={() => handlePokemonChange(pokemon.name)}>
               {pokemon.name}
@@ -36,7 +52,7 @@ console.log('render', pokemonList)
         ))}
       </ul>
 
-      {pokemonData && (
+      {pokemonData && 'name' in pokemonData && (
         <div>
           <h3>{pokemonData.name}</h3>
           <img src={pokemonData.sprites.front_default} alt={pokemonData.name} />
@@ -46,7 +62,7 @@ console.log('render', pokemonList)
       )}
 
       <div>
-        page:{page}
+        page: {page}
         <button onClick={() => handlePageChange(-1)}>prev</button>
         <button onClick={() => handlePageChange(1)}>next</button>
       </div>
