ae929569caebead07dcfcbb7101ecdaed4821073
yourunb <bxyura@gmail.com>
2025-01-16 17:49:57 +0300

    refactor: replace a href by Link

--- src/pages/Start/index.tsx
+++ src/pages/Start/index.tsx
@@ -12,29 +12,29 @@ const PageStart = () => {
           <Box>
             <Typography>
               This application is developed for educational purposes using technologies such as{' '}
-              <a href="https://react.dev/" target="_blank">
+              <Link to="https://react.dev/" target="_blank">
                 React
-              </a>
+              </Link>
               ,{' '}
-              <a href="https://redux.js.org/" target="_blank">
+              <Link to="https://redux.js.org/" target="_blank">
                 Redux
-              </a>
+              </Link>
               ,{' '}
-              <a href="https://redux-toolkit.js.org/rtk-query/overview" target="_blank">
+              <Link to="https://redux-toolkit.js.org/rtk-query/overview" target="_blank">
                 RTK Query
-              </a>
-              , <a href="https://vite.dev/guide/">Vite</a>,{' '}
-              <a href="https://mui.com/" target="_blank">
+              </Link>
+              , <Link to="https://vite.dev/guide/">Vite</Link>,{' '}
+              <Link to="https://mui.com/" target="_blank">
                 MUI
-              </a>{' '}
+              </Link>{' '}
               and{' '}
-              <a href="https://pokeapi.co/" target="_blank">
+              <Link to="https://pokeapi.co/" target="_blank">
                 PokeAPI
-              </a>
+              </Link>
               . The application is written in{' '}
-              <a href="https://www.typescriptlang.org/" target="_blank">
+              <Link to="https://www.typescriptlang.org/" target="_blank">
                 TypeScript
-              </a>{' '}
+              </Link>{' '}
               and has <a href="https://feature-sliced.design/">FSD</a> architecture.
             </Typography>
           </Box>
