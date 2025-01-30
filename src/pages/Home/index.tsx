c5fd2f0a4023be8d77aa5eda2c9277071f354b90
yourunb <bxyura@gmail.com>
2025-01-30 12:53:38 +0300

    feat: add custom pokemon modal

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { useFetchPokemonQuery, useFetchPokemonListQuery } from '../../store/apiSlice';
-import { Modal, Box, Button, Input, Typography } from '@mui/material';
+import { Box, Button, Input, Typography } from '@mui/material';
+import { ModalPokemon } from '../../shared/ui/modal/ModalPokemon';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateHistory } from '../../store/dataSlice';
+import { TPokemonData } from '../../shared/types';
 
 const theme = createTheme({
   palette: {
@@ -15,16 +17,6 @@ const theme = createTheme({
     },
   },
 });
-
-type TPokemonData = {
-  id: string,
-  name: string;
-  height: number;
-  weight: number;
-  sprites: {
-    front_default: string;
-  };
-};
 
 const PageHome: React.FC = () => {
   const dispatch = useDispatch();
@@ -150,33 +142,7 @@ const PageHome: React.FC = () => {
         </Box>
 
         {pokemonData && 'name' in pokemonData && (
-          <Modal open={openModal} onClose={handleCloseModal}>
-            <Box
-              sx={{
-                width: '200px',
-                position: 'absolute',
-                top: '50%',
-                left: '50%',
-                transform: 'translate(-50%, -50%)',
-                backgroundColor: 'white',
-                padding: 3,
-                borderRadius: 2,
-                boxShadow: 24,
-              }}
-            >
-              <Typography variant="h5" sx={{ textAlign: 'center', marginBottom: 2 }}>
-                {pokemonData?.name.toUpperCase()}
-              </Typography>
-              <img src={pokemonData?.sprites.front_default} alt={pokemonData?.name} style={{ width: '100px', height: '100px', display: 'block', margin: '0 auto' }} />
-              <Typography sx={{ marginTop: 2 }}>Height: {pokemonData?.height}</Typography>
-              <Typography>Weight: {pokemonData?.weight}</Typography>
-              <Box sx={{ textAlign: 'center', marginTop: 3 }}>
-                <Button variant="contained" color="primary" onClick={handleCloseModal}>
-                  Close
-                </Button>
-              </Box>
-            </Box>
-          </Modal>
+          <ModalPokemon openModal={openModal} handleCloseModal={handleCloseModal} pokemonData={pokemonData}/>
           )}
       </ThemeProvider>
     </Box>
---
+++ src/shared/types/index.ts
@@ -0,0 +1,9 @@
+export type TPokemonData = {
+  id: string,
+  name: string;
+  height: number;
+  weight: number;
+  sprites: {
+    front_default: string;
+  };
+};
