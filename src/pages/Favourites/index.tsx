5b4f93f6b426ac89fb6d64959e45b9aa32673bf9
yourunb <bxyura@gmail.com>
2025-01-30 11:30:12 +0300

    refactor: rename page

--- src/pages/Favourites/index.tsx
+++ src/pages/Favourites/index.tsx
@@ -1,4 +1,4 @@
-const PageStart = () => {
+const Favourites = () => {
   return (
     <div>
       Favourites page...
@@ -6,4 +6,4 @@ const PageStart = () => {
   );
 };
 
-export default PageStart;
+export default Favourites;
