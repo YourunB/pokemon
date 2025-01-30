6b13404feecf772018a8c6e7b654b874802aaedf
Yury Butskevich <124175026+YourunB@users.noreply.github.com>
2025-01-30 11:20:54 +0300

    feat: fill start page (#6)
    * feat: add module sass file
    
    * refactor: change start page style
    
    * refactor: replace a href by Link
    
    * feat: add link to logo

--- src/pages/Start/index.tsx
+++ src/pages/Start/index.tsx
@@ -1,49 +1,52 @@
 import pokemonLogo from '../../assets/images/logo-name.png';
 import { Link } from 'react-router-dom';
+import { Typography, Box } from '@mui/material';
+import s from './index.module.sass'
 
 const PageStart = () => {
   return (
-    <>
-      Page start...
-      <img src={pokemonLogo} className="logo react" alt="React logo" />
-      <ul>
-        <li>
-          <Link to={'/home'}>Home page</Link>
-        </li>
-        <li>
-          <Link to={'/about'}>About page</Link>
-        </li>
-      </ul>
-      <div>
-        <p>
-          This application is developed for educational purposes using technologies such as{' '}
-          <a href="https://react.dev/" target="_blank">
-            React
-          </a>
-          ,{' '}
-          <a href="https://redux.js.org/" target="_blank">
-            Redux
-          </a>
-          ,{' '}
-          <a href="https://redux-toolkit.js.org/rtk-query/overview" target="_blank">
-            RTK Query
-          </a>
-          , <a href="https://vite.dev/guide/">Vite</a>,{' '}
-          <a href="https://mui.com/" target="_blank">
-            MUI
-          </a>{' '}
-          and{' '}
-          <a href="https://pokeapi.co/" target="_blank">
-            PokeAPI
-          </a>
-          . The application is written in{' '}
-          <a href="https://www.typescriptlang.org/" target="_blank">
-            TypeScript
-          </a>{' '}
-          and has <a href="https://feature-sliced.design/">FSD</a> architecture.
-        </p>
-      </div>
-    </>
+    <div className={s.box}>
+      <Box sx={{display: 'flex', gap: '10px', maxWidth: '1280px', margin: 'auto', minHeight: 'calc(100vh - 20px)', alignItems: 'center'}}>
+        <Box sx={{textAlign: 'center'}}>
+          <Link to={'/home'}>
+            <img src={pokemonLogo} className={s.logo} alt="React logo" />
+          </Link>
+          <Box>
+            <Typography>
+              This application is developed for educational purposes using technologies such as{' '}
+              <Link to="https://react.dev/" target="_blank">
+                React
+              </Link>
+              ,{' '}
+              <Link to="https://redux.js.org/" target="_blank">
+                Redux
+              </Link>
+              ,{' '}
+              <Link to="https://redux-toolkit.js.org/rtk-query/overview" target="_blank">
+                RTK Query
+              </Link>
+              , <Link to="https://vite.dev/guide/">Vite</Link>,{' '}
+              <Link to="https://mui.com/" target="_blank">
+                MUI
+              </Link>{' '}
+              and{' '}
+              <Link to="https://pokeapi.co/" target="_blank">
+                PokeAPI
+              </Link>
+              . The application is written in{' '}
+              <Link to="https://www.typescriptlang.org/" target="_blank">
+                TypeScript
+              </Link>{' '}
+              and has <a href="https://feature-sliced.design/">FSD</a> architecture.
+            </Typography>
+          </Box>
+          <Typography sx={{display: 'flex', gap: '10px', justifyContent: 'center'}}>
+            <Link to={'/home'}>START</Link>
+            <Link to={'/about'}>ABOUT</Link>
+          </Typography>
+        </Box>
+      </Box>
+    </div>
   );
 };
 
